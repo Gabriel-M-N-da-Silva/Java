a3cfb57a36150d1a6ce60fca145c3403c3aaaed1
Gabriel <86237777+Gabriel-M-N-da-Silva@users.noreply.github.com>
2021-09-20 08:26:26 -0300

    Update TesteArrayList.java

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/TesteArrayList.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/TesteArrayList.java
@@ -1,7 +1,6 @@
 package br.com.bytebank.banco.teste.util;
 
 import java.util.ArrayList;
-
 import br.com.bytebank.banco.modelo.Cliente;
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
